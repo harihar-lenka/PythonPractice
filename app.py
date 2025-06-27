7d8ffa6d295e1bfee60b9b9ad5c53c878dda45f7
harihar-lenka <hlenka.tech@gmail.com>
2025-06-27 20:40:02 +1000

    modifed app.py file

---
+++ app.py
@@ -0,0 +1,55 @@
+# print("Hello World")
+# print('*' * 10)
+# x = 1
+# y = 2
+
+
+############# Functions #############
+
+# def Hello():
+#     name = "Harihar"
+#     # print(name)
+#     return name
+
+
+# print(Hello())
+
+
+#########################################
+
+# def Name1():
+#     firstName = "Harihar"
+#     # print(firstName)
+#     return firstName
+#     Name2()
+
+
+# def Name2():
+#     lastName = "Lenka"
+#     # print(lastName)
+#     return lastName
+
+
+# print(Name1())
+# print(Name2())
+
+
+#######################   Nested Fucntions  ################################
+
+# def outerFunction(x):
+#     def innerFunction(y):
+#         return x + y
+#     return innerFunction
+
+
+# # output = outerFunction(10)
+# # print(output(10))
+
+# print(outerFunction(10)(10))
+
+######################################
+
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+    print("Five is greater than two!")
